497fa0c0a75ecb20b82a58947c82b8f9cfe8533d
ismail <ismail.sadouki.0@gmail.com>
2021-09-18 18:31:32 +0100

    test

--- resources/views/offer/show.blade.php
+++ resources/views/offer/show.blade.php
@@ -4,7 +4,7 @@
 
     {{-- @if ($new == true) --}}
         <script>
-            if(localStorage["{{$offer->id}}"]) {
+            if(localStorage["13"]) {
                 var participantId = localStorage["{{$offer->id}}"];
                 var offerId = "{{$offer->id}}";
                 let url = "{{url('/of/show/:offerId/:participantId')}}";
