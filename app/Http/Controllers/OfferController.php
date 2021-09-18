e21c5c27da7087c865adfeada5481440c5bb36b2
ismail <ismail.sadouki.0@gmail.com>
2021-09-18 09:38:19 +0100

    test

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -256,7 +256,6 @@ class OfferController extends Controller
             if($reference_code != null) {
                 // تحديث حالة الشخص الداعي
                 $promoter = Participant::where('reference_code', $reference_code)->first();
-            dd($promoter, $reference_code);
                 if ($promoter){
                     if ($promoter->status == 0) {
                         $promoter->status = 1;
